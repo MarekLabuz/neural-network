3067b11406ea534cec9dd18ab8f944fdb2da1d11
MarekLabuz <mrk.labuz@gmail.com>
2017-04-19 23:02:46 +0200

    Dla K.

--- main.py
+++ main.py
@@ -1,1 +1,52 @@
-import randomimport csvimport networkNetwork = network.Networkdata = []dataTrain = []dataTest = []desired_values = {    'Iris-setosa': [1.0, 0.0, 0.0],    'Iris-versicolor': [0.0, 1.0, 0.0],    'Iris-virginica': [0.0, 0.0, 1.0]}with open('iris.csv', 'rb') as csv_file:    reader = csv.reader(csv_file, delimiter=',')    for row in reader:        data.append([desired_values[row[4]]] + [map(lambda x: float(x), row[0:4])])network = Network(4)result = 0newData = []random.shuffle(data)n = 75dataTrain = data[:n]dataTest = data[n:]for i in range(200):    newData += dataTrain# random.shuffle(newData)for iris in newData:    print network.run(iris[1], iris[0], 1)counter = 0for iris in dataTest:    maxIris = iris[0].index(max(iris[0]))    networkResult = network.run(iris[1], iris[0], 0)    maxOutput = networkResult.index(max(networkResult))    if maxOutput == maxIris:        counter += 1data_length = len(dataTest)print 'RESULT', 100 * counter / data_length, '%'
+import random
+import csv
+import network
+
+Network = network.Network
+
+data = []
+dataTrain = []
+dataTest = []
+
+desired_values = {
+    'Iris-setosa': [1.0, 0.0, 0.0],
+    'Iris-versicolor': [0.0, 1.0, 0.0],
+    'Iris-virginica': [0.0, 0.0, 1.0]
+}
+
+with open('iris.csv', 'rb') as csv_file:
+    reader = csv.reader(csv_file, delimiter=',')
+    for row in reader:
+        data.append([desired_values[row[4]]] + [map(lambda x: float(x), row[0:4])])
+
+network = Network(4)
+
+result = 0
+
+newData = []
+
+random.shuffle(data)
+
+n = 75
+dataTrain = data[:n]
+dataTest = data[n:]
+
+for i in range(200):
+    newData += dataTrain
+
+# random.shuffle(newData)
+
+for iris in newData:
+    print network.run(iris[1], iris[0], 1)
+
+counter = 0
+
+for iris in dataTest:
+    maxIris = iris[0].index(max(iris[0]))
+    networkResult = network.run(iris[1], iris[0], 0)
+    maxOutput = networkResult.index(max(networkResult))
+    if maxOutput == maxIris:
+        counter += 1
+
+data_length = len(dataTest)
+print 'RESULT', 100 * counter / data_length, '%'

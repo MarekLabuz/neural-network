b142e34233556fb29641d56ca218f0d304d0992c
MarekLabuz <mrk.labuz@gmail.com>
2017-04-20 21:32:21 +0200

    Dla K.

--- som.py
+++ som.py
@@ -1,8 +1,12 @@
 import csv
 import neuron
+import random
+import math
 
 Neuron = neuron.Neuron
 Input = neuron.Input
+
+alpha = 1000
 
 with open('iris.csv', 'rb') as csv_file:
     reader = csv.reader(csv_file, delimiter=',')
@@ -23,8 +27,12 @@ class Node (Neuron):
 
 
 class Grid:
-    def __init__(self, n, _inputs):
+    def __init__(self, n, _inputs, initial_radius):
         self.grid = []
+        self.inputs = _inputs
+        self.initial_radius = initial_radius
+        self.t = 0
+        self.inputs_indexes = range(len(self.inputs))
         for i in range(n):
             self.grid.append([])
             for j in range(n):
@@ -32,16 +40,39 @@ class Grid:
 
     def __str__(self):
         result = ''
-        for r in self.grid:
+        for _row in self.grid:
             result += '| '
-            for node in r:
+            for node in _row:
                 result += str(node) + ' | '
             result += '\n'
 
         return result
 
+    def get_radius(self):
+        return self.initial_radius * math.exp(-self.t / alpha)
+
+    def get_random_sample(self, k=2):
+        return random.sample(self.inputs_indexes, k)
+
+    def get_winner(self, sample):
+        distances = []
+        for _row in self.grid:
+            for node in _row:
+                sum_dist = 0
+                for index in sample:
+                    sum_dist += math.pow(self.inputs[index].output_value - node.weights_inputs[index], 2)
+                dist = math.sqrt(sum_dist)
+                distances.append({'node': node, 'dist': dist})
+        return min(distances, key=lambda d: d['dist'])
+
+    def run(self):
+        random_sample = self.get_random_sample()
+        print self.get_winner(random_sample)
+
 
 inputs = [Input(3), Input(5), Input(1), Input(8)]
 grid = Grid(4, inputs)
 
-print grid
+grid.run()
+
+# print max([{'node': 'a', 'dist': 0}, {'node': 'b', 'dist': 3}, {'node': 'c', 'dist': -3}], key=lambda t: t['dist'])
